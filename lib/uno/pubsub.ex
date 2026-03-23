8370167beb91d9f005650239275e69ece1ef7a85
Alex Krantz <alex@krantz.dev>
2026-03-23 10:09:05 -0700

    Add pub/sub abstraction

---
+++ lib/uno/pubsub.ex
@@ -0,0 +1,19 @@
+defmodule Uno.PubSub do
+  @moduledoc """
+  Helpers for dispatching events along the PubSub broker
+  """
+
+  defp topic_name({:room, id}), do: "room:#{id}"
+  defp topic_name({:game, id}), do: "game:#{id}"
+
+  def subscribe(topic), do: Phoenix.PubSub.subscribe(__MODULE__, topic_name(topic))
+
+  def broadcast(topic, message),
+    do: Phoenix.PubSub.broadcast(__MODULE__, topic_name(topic), message)
+
+  def broadcast_from(from, topic, message),
+    do: Phoenix.PubSub.broadcast_from(__MODULE__, from, topic_name(topic), message)
+
+  def local_broadcast(topic, message),
+    do: Phoenix.PubSub.local_broadcast(__MODULE__, topic_name(topic), message)
+end
