8fcfac78bcccaf648d37d9a14197f2f7a2d01f9f
Alex Krantz <alex@krantz.dev>
2026-03-24 21:56:33 -0700

    Make card component globally accessible

--- lib/uno_web.ex
+++ lib/uno_web.ex
@@ -86,6 +86,7 @@ defmodule UnoWeb do
       import Phoenix.HTML
       # Core UI components
       import UnoWeb.CoreComponents
+      import UnoWeb.CardComponents
 
       # Common modules used in templates
       alias Phoenix.LiveView.JS
