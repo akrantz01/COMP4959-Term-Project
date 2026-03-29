af23e2c4f411a34abcf3bc69e15990a8b43b5920
Alex Krantz <alex@krantz.dev>
2026-03-29 16:55:16 -0700

    Temporarily subscribe to room and game topics

--- lib/uno_web/live/room_live.ex
+++ lib/uno_web/live/room_live.ex
@@ -4,6 +4,10 @@ defmodule UnoWeb.RoomLive do
   alias UnoWeb.RoomLive.{GameComponent, LobbyComponent}
 
   def mount(%{"id" => id}, _session, socket) do
+    # TODO: remove once room and game exist
+    PubSub.subscribe({:room, id})
+    PubSub.subscribe({:game, id})
+
     {:ok, socket |> assign(:id, id) |> assign(:state, :lobby)}
   end
 end
