bffa5067d445ae80dca72dd4aec25bd8d3a761c1
Alex Krantz <alex@krantz.dev>
2026-03-29 16:57:14 -0700

    Forward relevant events to child components

--- lib/uno_web/live/room_live.ex
+++ lib/uno_web/live/room_live.ex
@@ -1,6 +1,7 @@
 defmodule UnoWeb.RoomLive do
   use UnoWeb, :live_view
 
+  alias Uno.{Events, PubSub}
   alias UnoWeb.RoomLive.{GameComponent, LobbyComponent}
 
   def mount(%{"id" => id}, _session, socket) do
@@ -10,4 +11,27 @@ defmodule UnoWeb.RoomLive do
 
     {:ok, socket |> assign(:id, id) |> assign(:state, :lobby)}
   end
+
+  @forwarded_events %{
+    Events.PlayerJoined => :room,
+    Events.PlayerLeft => :room,
+    Events.GameStarted => :room,
+    Events.GameEnded => :room,
+    Events.Sync => :game,
+    Events.NextTurn => :game,
+    Events.CardsPlayed => :game,
+    Events.CardsDrawn => :game
+  }
+
+  def handle_info(%mod{} = msg, socket) when is_map_key(@forwarded_events, mod) do
+    with {component, id} <- component_target(socket.assigns.state, @forwarded_events[mod]),
+         do: send_update(component, id: id, event: msg)
+
+    {:noreply, socket}
+  end
+
+  defp component_target(state, :both), do: component_target(state, state)
+  defp component_target(:room, :room), do: {RoomComponent, "lobby"}
+  defp component_target(:game, :game), do: {GameComponent, "game"}
+  defp component_target(_state, _spec), do: nil
 end
