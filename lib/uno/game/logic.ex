1ab24049e264da7e815e28ca1ad1ebf42cdb47f1
Peter <jableno672@proton.me>
2026-04-13 23:53:12 -0700

    Implement generate_deck()

--- lib/uno/game/logic.ex
+++ lib/uno/game/logic.ex
@@ -2,4 +2,34 @@ defmodule Uno.Game.Logic do
   @moduledoc """
   The core UNO logic
   """
+  @type colour :: :red | :green | :blue | :yellow
+  @type card_type :: 0..9 | :reverse | :skip | :draw_2
+  @type card_wild :: :wild | :wild_draw_4
+  @type hand_card :: {colour(), card_type()} | card_wild()
+  @type played_card :: {colour(), card_type()} | {card_wild(), colour()}
+
+  # task 1
+  @spec generate_deck() :: [hand_card()]
+  def generate_deck do
+    colours = [:red, :green, :blue, :yellow]
+
+    colour_cards =
+      for colour <- colours do
+        zeros = [{colour, 0}]
+
+        duplicated =
+          for type <- [1..9 |> Enum.to_list(), :reverse, :skip, :draw_2] |> List.flatten(),
+              _copy <- 1..2 do
+            {colour, type}
+          end
+
+        zeros ++ duplicated
+      end
+
+    wildcards = List.duplicate(:wild, 4) ++ List.duplicate(:wild_draw_4, 4)
+
+    colour_cards |> List.flatten() |> Kernel.++(wildcards)
+  end
+
+
 end
