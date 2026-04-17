91567af9ef358774c51abd03372180f177839efa
jiahong <jiahongxu1997@gmail.com>
2026-04-16 23:00:41 -0700

    format error

--- lib/uno_web/live/room_live.ex
+++ lib/uno_web/live/room_live.ex
@@ -1,7 +1,7 @@
 defmodule UnoWeb.RoomLive do
   use UnoWeb, :live_view
-
-  # alias UnoWeb.RoomLive.{GameComponent, LobbyComponent}
+  alias UnoWeb.Forms.RoomForm
+  alias UnoWeb.RoomLive.{GameComponent, LobbyComponent}
 
   def mount(%{"room_id" => room_id}, _session, socket) do
     player_id = socket.assigns[:player_id] || Nanoid.generate()
@@ -26,27 +26,6 @@ defmodule UnoWeb.RoomLive do
          state: :lobby
        })
      )}
-  end
-
-  ## HANDLE PUBSUB EVENTS
-
-  def handle_info({:player_joined, player_id}, socket) do
-    {:noreply, update(socket, :players, fn p -> Enum.uniq([player_id | p]) end)}
-  end
-
-  def handle_info({:player_left, player_id}, socket) do
-    {:noreply, update(socket, :players, fn p -> List.delete(p, player_id) end)}
-  end
-
-  def handle_info({:game_started, _}, socket) do
-    {:noreply, assign(socket, status: :playing)}
-  end
-
-  def handle_info({:game_ended, _}, socket) do
-    {:noreply, assign(socket, status: :ended)}
-    alias Uno.{Events, PubSub}
-    alias UnoWeb.Forms.RoomForm
-    alias UnoWeb.RoomLive.{GameComponent, LobbyComponent}
   end
 
   # --- Temporary, remove once more is implemented ---
@@ -80,6 +59,24 @@ defmodule UnoWeb.RoomLive do
     Events.CardsPlayed => :game,
     Events.CardsDrawn => :game
   }
+
+  ## HANDLE PUBSUB EVENTS
+
+  def handle_info({:player_joined, player_id}, socket) do
+    {:noreply, update(socket, :players, fn p -> Enum.uniq([player_id | p]) end)}
+  end
+
+  def handle_info({:player_left, player_id}, socket) do
+    {:noreply, update(socket, :players, fn p -> List.delete(p, player_id) end)}
+  end
+
+  def handle_info({:game_started, _}, socket) do
+    {:noreply, assign(socket, status: :playing)}
+  end
+
+  def handle_info({:game_ended, _}, socket) do
+    {:noreply, assign(socket, status: :ended)}
+  end
 
   def handle_info(%mod{} = msg, socket) when is_map_key(@forwarded_events, mod) do
     with {component, id} <- component_target(socket.assigns.state, @forwarded_events[mod]),
