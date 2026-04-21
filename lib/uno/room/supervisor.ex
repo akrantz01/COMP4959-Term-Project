abe98df734a085aace19a2fe21e3699d1f4c839c
Alex Krantz <alex@krantz.dev>
2026-04-20 17:26:19 -0700

    Allow looking up room by ID

--- lib/uno/room/supervisor.ex
+++ lib/uno/room/supervisor.ex
@@ -19,6 +19,17 @@ defmodule Uno.Room.Supervisor do
   # Public API
 
   @doc """
+  Check if a room with the given `room_id` exists
+  """
+  @spec lookup(String.t()) :: {:ok, pid()} | {:error, :not_found}
+  def lookup(room_id) do
+    case Registry.lookup(Uno.Room.Registry, room_id) do
+      [{pid, nil}] -> {:ok, pid}
+      [] -> {:error, :not_found}
+    end
+  end
+
+  @doc """
   Start a new room with the given `room_id` or a random one if not provided.
 
   Returns
